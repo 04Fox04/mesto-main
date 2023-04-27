630dfa2d8542d1a6aafdf3e47c505049153cdcf5
Kirill <117633274+04Fox04@users.noreply.github.com>
2023-04-27 12:11:23 +0300

    Delete src/scripts directory

--- src/scripts/Components/Section.js
+++
@@ -1,9 +0,0 @@
-export default class Section {
-  constructor({ renderer }, selectorContainer) {
-    this._renderer = renderer;
-    this._selectorContainer = selectorContainer;
-  }
-  addItem(element) {
-    this._selectorContainer.prepend(element);
-  }
-}
